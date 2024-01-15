0972edf21ea0c5cc4b80330b3fdb6e0abefb534c
Comies34 <come.bonal@gmail.com>
2024-01-15 02:35:43 +0100

    mise en place système de cookies

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -10,12 +10,21 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Cookie;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, AteliersController $ateliersController, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
     {
+        $data = $ateliersController->getAteliers($entityManager)->getContent();
+        $data = json_decode($data, true);
+
+        if ($request->cookies->has('user_id')) {
+            return $this->redirectToRoute('Home');
+
+        }
+
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
         $form->handleRequest($request);
@@ -33,11 +42,21 @@ class RegistrationController extends AbstractController
             $entityManager->flush();
             // do anything else you need here, like send an email
 
-            return $this->redirectToRoute('ateliers');
+            $userId = $user->getId();
+
+
+            // Create the response object and set the cookie
+            $cookie = Cookie::create('user_id', $userId, strtotime('+1 week'));
+            $response = $this->redirectToRoute('Home');
+            $response->headers->setCookie($cookie);
+
+            return $response;
         }
 
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
     }
+
+
 }
--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,27 +6,71 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Cookie;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+
 
 class SecurityController extends AbstractController
 {
-    #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils): Response
+    #[Route('/login', name: 'app_login')]
+    public function login(Request $request, AteliersController $ateliersController, AuthenticationUtils $authenticationUtils, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        $data = $ateliersController->getAteliers($entityManager)->getContent();
+        $data = json_decode($data, true);
 
-        // get the login error if there is one
+        // Check if the user is already logged in (has user_id cookie)
+        if ($request->cookies->has('user_id')) {
+            return $this->redirectToRoute('Home');
+        }
+
+        // Get the last login error, if any
         $error = $authenticationUtils->getLastAuthenticationError();
-        // last username entered by the user
+
+        // Get the last username entered by the user
         $lastUsername = $authenticationUtils->getLastUsername();
 
-        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+        // Render the login form
+        $response = $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+
+        // Check if the user just successfully logged in
+        if ($this->getUser() && !$request->cookies->has('user_id')) {
+            // Get the user ID
+            $userId = $this->getUser()->getId();
+
+            // Create and set the user_id cookie
+            $cookie = Cookie::create('user_id', $userId, strtotime('+1 week'));
+            $response->headers->setCookie($cookie);
+
+            // Send headers immediately
+            $response->sendHeaders();
+        }
+
+        // If the user is not logged in or the cookie is already set, continue to display the login page
+        return $response;
     }
 
-    #[Route(path: '/logout', name: 'app_logout')]
-    public function logout(): void
+
+
+
+    #[Route('/deconnexion', name: 'deconnexion')]
+    public function logout(AteliersController $ateliersController, Request $request, EntityManagerInterface $entityManager): Response
     {
-        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+        // Get data
+        $data = $ateliersController->getAteliers($entityManager)->getContent();
+        $data = json_decode($data, true);
+
+        // Create a Response object
+
+        $response = new Response();
+        // Clear the 'user_id' cookie on the Response object
+        $response = $this->redirectToRoute('Home');
+        $response->headers->clearCookie('user_id', '/', null);
+
+
+        return $response;
+
     }
 }
